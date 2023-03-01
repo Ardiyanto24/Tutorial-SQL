d7827661721826b7d90cb43b05879832c50de389
Ardiyanto24 <117892532+Ardiyanto24@users.noreply.github.com>
2023-03-01 14:01:35 +0700

    Add files via upload

---
+++ 1. Database.sql
@@ -0,0 +1,12 @@
+#===== MEMBUAT DATABASE =====
+CREATE database Statistika;
+
+# ===== MELIHAT DATABASE ====
+SHOW databases;
+
+# ==== MENGGUNAKAN DATABASE ====
+# sebelum memulai menjalan query pada MySQL atapun yang lain perlu untuk memilih database terlebih dahulu untuk digunakan
+USE statistika; # namu jika menggunakan MySQL juga dengan double klik daftar database
+
+# ==== MENGHAPUS DATABASE =====
+DROP DATABASE statistika;
